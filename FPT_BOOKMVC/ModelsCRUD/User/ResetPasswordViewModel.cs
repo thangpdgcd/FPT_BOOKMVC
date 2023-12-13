b34da84b09b9ae3f6a36c6afe8e2106be16934d2
Phan Đình Thắng <106921106+thangpdgcd@users.noreply.github.com>
2023-12-13 15:11:44 +0700

    mvc User

---
+++ FPT_BOOKMVC/ModelsCRUD/User/ResetPasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication123.ModelsCRUD.User;
+
+public class ResetPasswordViewModel
+{
+    [Required] 
+    [EmailAddress] public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Password and confirm password must match")]
+    public string ConfirmPassword { get; set; }
+
+    // public string Token { get; set; }
+    
+}
